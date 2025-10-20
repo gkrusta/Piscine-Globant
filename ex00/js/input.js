0d3b23c54a6c9779d21dc91fbd8a1560d76a13a5
gkrusta <krustagabriela@gmail.com>
2025-10-21 00:19:48 +0200

    feat: arrow key movements

--- ex00/js/input.js
+++ ex00/js/input.js
@@ -11,13 +11,73 @@ document.addEventListener('keydown', (e) => {
 });
 
 function moveUp() {
-	for (let c; c < GRID_SIZE; c++) {
-		let col = []
+	for (let c = 0; c < GRID_SIZE; c++) {
+		let col = [];
 
-		for (let r; r < GRID_SIZE; r++) {
-			if (grid[r][c] != 0) col.push(grid[r][c]) 
+		for (let r = 0; r < GRID_SIZE; r++) {
+			if (grid[r][c] != 0)
+				col.push(grid[r][c]);
 		}
-		
+		while (col.length < GRID_SIZE)
+			col.push(0);
+		for (let r = 0; r < GRID_SIZE; r++) {
+			grid[r][c] = col[r];
+		}
 	}
-	addRandomCell()
+	addRandomCell();
+	render();
+}
+
+function moveDown() {
+	for (let c = 0; c < GRID_SIZE; c++) {
+		let col = [];
+
+		for (let r = 0; r < GRID_SIZE; r++) {
+			if (grid[r][c] != 0)
+				col.push(grid[r][c]);
+		}
+		while (col.length < GRID_SIZE)
+			col.unshift(0);
+		for (let r = 0; r < GRID_SIZE; r++) {
+			grid[r][c] = col[r];
+		}
+	}
+	addRandomCell();
+	render();
+}
+
+function moveLeft() {
+	for (let r = 0; r < GRID_SIZE; r++) {
+		let row = [];
+
+		for (let c = 0; c < GRID_SIZE; c++) {
+			if (grid[r][c] != 0)
+				row.push(grid[r][c]);
+		}
+		while (row.length < GRID_SIZE)
+			row.push(0);
+		for (let c = 0; c < GRID_SIZE; c++) {
+			grid[r][c] = row[c];
+		}
+	}
+	addRandomCell();
+	render();
+}
+
+function moveRight() {
+	for (let r = 0; r < GRID_SIZE; r++) {
+		let row = [];
+
+		for (let c = 0; c < GRID_SIZE; c++) {
+			if (grid[r][c] != 0)
+				row.push(grid[r][c]);
+		}
+		while (row.length < GRID_SIZE)
+			row.unshift(0);
+		for (let c = 0; c < GRID_SIZE; c++) {
+			grid[r][c] = row[c];
+		}
+	}
+	addRandomCell();
+	render();
 }
